5887cc198fd93c502eadc715a4ee236f509e0769
onuchukwu captain nwanne <onuchukwucaptain@gmail.com>
2022-10-16 19:36:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *return: always 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return(1)
+}
